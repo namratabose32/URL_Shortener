287c6b9da9eda2b2129888d360d63573135da328
Namrata <namratabose32@gmail.com>
2024-01-04 00:07:05 +0530

    Url analytics

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express=require('express');
 const app=express();
 
 const {connectToMongoDB}=require('./connect');
-const urlRoute=require('./routes/url')
+const urlRoute=require('./routes/url');
+const URL = require('./models/url');
 
 const PORT=8001;
 
@@ -11,5 +12,17 @@ connectToMongoDB('mongodb+srv://namratabose322:namratabose32@cluster0.hfublfb.mo
 app.use(express.json());
 
 app.use("/url",urlRoute);
+
+app.get('/:shortId', async (req,res)=>{
+    const shortId=req.params.shortId;
+    const entry=await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+        visitHistory:{
+            timestamp:Date.now(),
+        }
+    }})
+    res.redirect(entry.redirectURL);
+})
 
 app.listen(PORT,()=>console.log(`Server running on PORT :${PORT}`))
