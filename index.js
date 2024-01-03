84dfe13285a5d40bdf5abe07aeb6894802caf683
Namrata <namratabose32@gmail.com>
2024-01-03 23:41:46 +0530

    POST method created

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express=require('express');
 const app=express();
+
+const {connectToMongoDB}=require('./connect');
+const urlRoute=require('./routes/url')
+
 const PORT=8001;
+
+connectToMongoDB('mongodb+srv://namratabose322:namratabose32@cluster0.hfublfb.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log("MongoDb connected"));
+
+app.use(express.json());
+
+app.use("/url",urlRoute);
 
 app.listen(PORT,()=>console.log(`Server running on PORT :${PORT}`))
