20592562bc79eb933ed30bd0e997ba26f13520dc
Namrata <namratabose32@gmail.com>
2024-01-04 21:08:11 +0530

    Login Page created

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const {v4:uuidv4}=require('uuid')
 async function handleUserSignup(req,res){
     const {name,email,password}=req.body;
     await User.create({
@@ -9,5 +9,13 @@ async function handleUserSignup(req,res){
     });
     return res.render("home");
 }
-
-module.exports={handleUserSignup}
+async function handleUserLogin(req,res){
+    const {email,password}=req.body;
+    const user=await User.findOne({email,password});
+    if(!user)
+        return res.render("login",{
+            error: "Invalid Email or password"
+        })
+    return res.render("home");
+}
+module.exports={handleUserSignup,handleUserLogin}
